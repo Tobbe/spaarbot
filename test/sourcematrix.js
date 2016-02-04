7bc7c164101e18195d88f4533869e33a4a3ecb3b
Tobbe Lundberg <tobbe@tlundberg.com>
2016-02-04 21:49:42 +0100

    SourceMatrix init method doesn't take a parameter any more

--- test/sourcematrix.js
+++ test/sourcematrix.js
@@ -2,14 +2,14 @@ eval(require('fs').readFileSync('sourcematrix.js', 'utf8'));
 
 describe('SourceMatrix', function () {
     it('can store one VisualCmd', function () {
-        var sourceMatrix = Object.create(SourceMatrix).init(10);
+        var sourceMatrix = Object.create(SourceMatrix).init();
         sourceMatrix.addCommand(Object.create(VisualCmdRight));
         expect(sourceMatrix.toArray().length).toEqual(1);
         expect(VisualCmdRight.isPrototypeOf(sourceMatrix.toArray()[0])).toEqual(true);
     });
 
     it('can store several VisualCmds', function () {
-        var sourceMatrix = Object.create(SourceMatrix).init(10);
+        var sourceMatrix = Object.create(SourceMatrix).init();
         sourceMatrix.addCommand(Object.create(VisualCmdLeft));
         sourceMatrix.addCommand(Object.create(VisualCmdLeft));
         sourceMatrix.addCommand(Object.create(VisualCmdLeft));
@@ -22,7 +22,7 @@ describe('SourceMatrix', function () {
     });
 
     it('can store several VisualCmds, spanning several rows', function () {
-        var sourceMatrix = Object.create(SourceMatrix).init(2);
+        var sourceMatrix = Object.create(SourceMatrix).init();
         sourceMatrix.addCommand(Object.create(VisualCmdLeft));
         sourceMatrix.addCommand(Object.create(VisualCmdLeft));
         sourceMatrix.addCommand(Object.create(VisualCmdLeft));
